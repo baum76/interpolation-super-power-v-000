92ff6cfc177616bc264e9594cb3d2f6504ef8af8
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-12 23:53:16 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,5 +3,3 @@ def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indogo', 'viol
   puts "R: #{[0]}, O: #{[1]}, Y: #{[2]}, G: #{[3]}, B: #{[4]}, I: #{[5]}, V: #{[6]}"
 
 end
-
-display_rainbow
